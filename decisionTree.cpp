1f1b97f8c354e03eb7e34361bf0ebc435f14425b
Mukul Madaan <mukul.madaan8@gmail.com>
2019-11-07 19:27:18 +0530

    Update decisionTree.cpp

--- decisionTree.cpp
+++ decisionTree.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//Class to create the node of decision tree
 class decisionTree {
 	
 	public:
@@ -11,12 +12,16 @@ class decisionTree {
 		
 };
 
+//Create New Node
 decisionTree *newNode() 
 { 
+
     decisionTree *temp = new decisionTree(); 
-    return temp; 
+    return temp;
+	 
 }
  
+//Print the group
 void printInfo(decisionTree* root, int x) {
 	
 	decisionTree* current = root->group[x];
@@ -26,6 +31,7 @@ void printInfo(decisionTree* root, int x) {
 	
 }
 
+//Print the group in sorted order
 void sortedprintInfo(decisionTree* root, int x) {
 	
 	decisionTree* current = root->group[x];
@@ -34,10 +40,25 @@ void sortedprintInfo(decisionTree* root, int x) {
 		cout<<current->element[i]<<" ";
 	}
 	
+}
+
+//Find the group number
+void findGroup(int number) {
+	
+	cout<<"The Group Number in which the element is: "<<number%10;
 	
 }
 
-void createGroup(decisionTree* root,int input[32][32]) {
+//Enter a New number
+void newElement(decisionTree* root, int number){
+	
+	int groupNumber = number%10;
+	root->group[groupNumber]->element.push_back(number);
+	
+}
+
+//Logic to create the group
+void createGroup(decisionTree* root, int input[32][32]) {
 
 	for(int i = 0; i < 10; i++){
 		root->group[i] = newNode();
@@ -54,24 +75,32 @@ void createGroup(decisionTree* root,int input[32][32]) {
 	
 }
 
+//Main Funtion call
 int main() { 
 
 	int input[32][32];
 	
 	decisionTree* root = new decisionTree();
 	root->groupName = "Decision Tree";
+	
 	for(int i = 0; i < 32; i++) {
 		for(int j = 0; j < 32; j++) {
 			input[i][j] = rand()%1025;
 			
 		}
 	}
-	createGroup(root, input);
 	
-	int gn;
-	cout<<"Enter the Group that you want to display:\n";
-	cin>>gn;
-	printInfo(root,gn);
-	cout<<"\nOkay. Now in an organised fashion\n";
-	sortedprintInfo(root,gn);
+	createGroup(root, input);
+	findGroup(33);
+	int gNumber;
+	int flag = 1;
+	while(flag){
+		cout<<"\nEnter the Group that you want to display:\t";
+		cin>>gNumber;
+		printInfo(root,gNumber);
+		cout<<"\nOkay. Now in an organised fashion\n";
+		sortedprintInfo(root,gNumber);
+		cout<<"\nDo you Wish to Continue?\t";
+		cin>>flag;
+	}
 }
